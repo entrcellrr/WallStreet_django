4ffbccf19a1c5edc2560b42de8948368769b69de
S K Aravind <aravindraghvi@gmail.com>
2016-06-12 15:03:33 +0530

    initial commit

---
+++ login/views.py
@@ -0,0 +1,46 @@
+from login.forms import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.views.decorators.csrf import csrf_protect
+from django.shortcuts import render_to_response, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.template import RequestContext
+ 
+@csrf_protect
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+            username=form.cleaned_data['username'],
+            password=form.cleaned_data['password1'],
+            email=form.cleaned_data['email']
+            )
+            return HttpResponseRedirect('/success/')
+    else:
+        form = RegistrationForm()
+    variables = RequestContext(request, {
+    'form': form
+    })
+ 
+    return render_to_response(
+    'registration/register.html',
+    variables,
+    )
+ 
+def registersuccess(request):
+    return render_to_response(
+        'registration/success.html',
+        )
+
+
+def logout_page(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+ 
+@login_required
+def home(request):
+    return render_to_response(
+    'login/home.html',
+    { 'user': request.user }
+    )
