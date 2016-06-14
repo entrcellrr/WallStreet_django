a914f89b8b5949067da2938ce65106fabca3d587
S K Aravind <aravindraghvi@gmail.com>
2016-06-14 22:52:04 +0530

    QTY box, few more changes

--- sellbuy/views_sellbuy.py
+++ sellbuy/views_sellbuy.py
@@ -6,17 +6,19 @@ from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render_to_response, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext
-from sellbuy.models import Share
+from sellbuy.models import Share, ShareDetail
 from login.models import UserDetail
 # Create your views here.
 @csrf_protect
 @login_required
 def sellbuyhome(request):
 	queryset=Share.objects.all()
+	share=Share.objects.all()
 	user = UserDetail.objects.get(name=request.user)
 	context_data={
-	"id":user.id,
-	"object_list":queryset
+		"id":user.id,
+		"object_list":queryset,
+		"shares":share
 	}
 	return render_to_response(
     	'sellbuy/transact.html',context_data
