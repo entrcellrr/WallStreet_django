e2d724c7eea6a0851c165b48da7fb1ad308859fb
aditya <aditya999123@gmail.com>
2016-06-14 20:47:21 +0530

    drop down list implemented

--- sellbuy/views_sellbuy.py
+++ sellbuy/views_sellbuy.py
@@ -6,16 +6,17 @@ from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render_to_response, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext
-from sellbuy.models import ShareDetail
+from sellbuy.models import Share
 from login.models import UserDetail
 # Create your views here.
 @csrf_protect
 @login_required
 def sellbuyhome(request):
-	#listnames=UserDetail.objects.all().values_list('name')
+	queryset=Share.objects.all()
 	user = UserDetail.objects.get(name=request.user)
 	context_data={
-	"id":user.id
+	"id":user.id,
+	"object_list":queryset
 	}
 	return render_to_response(
     	'sellbuy/transact.html',context_data
