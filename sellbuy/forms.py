9be7725e21741a1ba30884ab89aeeccb10611b53
aditya <aditya999123@gmail.com>
2016-06-15 15:36:12 +0530

    forms and list using .py

--- sellbuy/forms.py
+++ sellbuy/forms.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from .models import Share
-
+from sellbuy.models import Share, ShareDetail
+from login.models import UserDetail
 class ShareForm(forms.ModelForm):
     class Meta:
         model=Share
         fields=['name','describ','currentprice']
+queryset=Share.objects.values_list('describ','describ').order_by('describ').distinct()
+    
+class ListForm(forms.Form):
+    #queryset=Share.objects.all()
 
-
-
+    relevance = forms.ChoiceField(#initial=prev,
+    	widget=forms.Select(),required=True,choices=[(o) for o in queryset])
+       
 def clean_name(self):
     name= self.cleaned_data.get('name')
     return name
--- sellbuy/views_sellbuy.py
+++ sellbuy/views_sellbuy.py
@@ -8,42 +8,16 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext
 from sellbuy.models import Share, ShareDetail
 from login.models import UserDetail
+from .forms import ListForm
 # Create your views here.
 @login_required
 def sellbuyhome(request):
-	user = UserDetail.objects.get(name=request.user)
-	shares = Share.objects.all()
-	id = user.name
-	attr_filter=request.GET.get("ShareDescrib")
-	attr = request.GET.get("branch")
-	if attr is not None:
-		model = ShareDetail.objects.get(username=id)
-		qty2 = int(request.GET.get("QTY"))
-		qty1 = getattr(model, attr)
-		if request.GET.get("sell") == 'SELL':
-			if qty2 > qty1:
-				qty2 = qty1
-			qty2 = -qty2
-		setattr(model, attr, qty1+qty2)
-		model.save()
-	queryset=Share.objects.all()
-	flshare=Share.objects.filter(describ=attr_filter)
-	flshare1=queryset 
-	context_data={
-		"name":user.name,
-		"describ_list":queryset,
-		"share_filtered":flshare
-	}
-	context_default={
-		"name":user.name,
-		"describ_list":queryset,
-		"share_filtered":flshare1
-	}
-	if attr_filter is not None:
-		return render_to_response(
-   	    	'sellbuy/transact.html',context_data
-     		)
-	else:
-		return render_to_response(
-			'sellbuy/transact.html',context_default
-			)
+	
+	
+	form = ListForm()
+	variables = RequestContext(request,{
+    	'form':form
+    	})
+	return render_to_response(
+   	    'sellbuy/transact.html',variables,
+     	)
