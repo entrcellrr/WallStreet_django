30d2fe80bc887f9e24ff8ec5bc87b01dcd67d2d3
S K Aravind <aravindraghvi@gmail.com>
2016-06-28 15:52:33 +0530

    Portfolio table fixed

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Share_Data(models.Model):
     x=models.DecimalField(max_digits=7,decimal_places=2,default=0.00)
     y=models.DecimalField(max_digits=7,decimal_places=2,default=0.00)
-    def __str__(self):
-    	return self.x
+    #def __str__(self):
+    	#return str(self.x)
