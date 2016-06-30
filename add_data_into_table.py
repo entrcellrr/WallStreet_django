c7efdbc0d3d549e9d1857ec3a7ad4f03489e6114
S K Aravind <aravindraghvi@gmail.com>
2016-06-30 17:12:16 +0530

    Real data for google synced

---
+++ add_data_into_table.py
@@ -0,0 +1,20 @@
+from portfolio.models import *
+import csv
+with open('Facebook.csv') as f:
+	reader = csv.reader(f,delimiter=',')
+	for row in reader:
+		newnumber = ''
+		for o in row[1]:
+			if o != ',':
+				newnumber+=o
+		Google.objects.create(x=row[0][0:10],y=float(newnumber))
+
+with open('Google.csv') as f:
+	reader = csv.reader(f,delimiter=',')
+	for row in reader:
+		newnumber = ''
+		for o in row[1]:
+			if o != ',':
+				newnumber+=o
+		Facebook.objects.create(x=row[0][0:10],y=float(newnumber))
+
